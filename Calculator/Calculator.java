79855b555d4f27a1fec43d72c4588581ed096079
UditBhatt1 <uditbhatt.work@gmail.com>
2023-12-06 11:39:01 +0530

    First commit

---
+++ Calculator/Calculator.java
@@ -0,0 +1,94 @@
+package Calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    int x;
+    int y;
+
+    public void input() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("1.Addition of two numbers.");
+        System.out.println("2.Substration of two numbers.");
+        System.out.println("3.Muliplication of two numbers.");
+        System.out.println("4.Division of two numbers.");
+        System.out.println("5.Exit.");
+        System.out.print("Enter your choice: ");
+
+        int input = scan.nextInt();
+
+        switch (input) {
+            case 1:
+                addition();
+                break;
+            case 2:
+                substraction();
+                break;
+            case 3:
+                multiplication();
+                break;
+            case 4:
+                division();
+                break;
+            default:
+                System.out.println("Exited the program");
+                break;
+
+        }
+    }
+
+    public void addition() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the value of a: ");
+        int a = scan.nextInt();
+
+        System.out.println("Enter the value of b: ");
+        int b = scan.nextInt();
+
+        int c = a + b;
+        System.out.println("The addition of " + a + " + " + b + " is: " + c);
+    }
+
+    public void substraction() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the value of a: ");
+        int a = scan.nextInt();
+
+        System.out.println("Enter the value of b: ");
+        int b = scan.nextInt();
+
+        int c = a - b;
+        System.out.println("The substraction of " + a + " - " + b + " is: " + c);
+    }
+
+    public void multiplication() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the value of a: ");
+        int a = scan.nextInt();
+
+        System.out.println("Enter the value of b: ");
+        int b = scan.nextInt();
+
+        int c = a * b;
+        System.out.println("The multiplication of " + a + " * " + b + " is: " + c);
+    }
+
+    public void division() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the value of a: ");
+        int a = scan.nextInt();
+
+        System.out.println("Enter the value of b: ");
+        int b = scan.nextInt();
+
+        if (b != 0) {
+            int c = a / b;
+            System.out.println("The division of " + a + " / " + b + " is: " + c);
+        } else
+            System.out.println("Cannot divide by zero");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+import Calculator.Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.input();
+    }
+
+}
